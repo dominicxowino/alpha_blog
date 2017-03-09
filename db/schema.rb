675c43d9c57062330a9424e8c297d0937774cc35
Dominic Owino <dominic7owino@yahoo.com>
2017-03-09 05:07:06 +0000

    validations

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@
 ActiveRecord::Schema.define(version: 20170308104043) do
 
   create_table "articles", force: :cascade do |t|
-    
-    
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
